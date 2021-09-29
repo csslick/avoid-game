6c7257f2f5097488d73a1af516a47049fcaeb7b1
csslick <digitone@naver.com>
2021-09-30 00:04:04 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -2,8 +2,21 @@
 $(function(){
 
   var enemyMove = setInterval(function(){
-    var top = Math.round($('.dunk').eq(0).position().top);
-    // console.log(top)
+    // 충돌 채크
+    for(var i = 0; i < $('.dunk').length; i++) {
+      var eTop = Math.round($('.dunk').eq(i).position().top);
+      var eLeft = Math.round($('.dunk').eq(i).position().left);
+      var myTop = Math.round($('.bird').position().top);
+      var myLeft = Math.round($('.bird').position().left);
+      if(eTop+200 > myTop && eLeft < myLeft+64 && eLeft+50 > myLeft) {
+        console.log('You Lose!');
+        $('.bird').addClass('dead');
+        // clearInterval(enemyMove);
+        // location.reload();
+      } else {
+        // $('.bird').removeClass('dead');
+      }
+    }
   }, 1000/15)
 
   $('#moveBtn').click(function(){
